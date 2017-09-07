e31edbb9fd745b8b140f3066cd904ae656ba1f76
Swarn <swarndeep@cerebralfix.com>
2017-09-08 11:21:21 +1200

    check

--- MOBApps/src/test/java/Setup/SD.java
+++ MOBApps/src/test/java/Setup/SD.java
@@ -5,7 +5,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-import static com.codeborne.selenide.Selenide.page;
+//import static com.codeborne.selenide.Selenide.page;
 import static junit.framework.TestCase.assertTrue;
 
 /**
